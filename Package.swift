15074d058069c8c935a8946f401486c03de8dc31
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2025-10-14 21:26:58 +0200

    Turned library into dynamic one

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "MLXUtilsLibrary",
-            type: .static,
             targets: ["MLXUtilsLibrary"]
         ),
     ],
